a797d6247cfa1c0eb3362b2b1ac245b55ee1cf20
KimmyDev <iroda.kodirova@billz.uz>
2022-07-18 21:08:33 +0500

    chat app using channel

---
+++ chat/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for chat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+import chat_app.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+  "http": django_asgi_app,
+  "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat_app.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
